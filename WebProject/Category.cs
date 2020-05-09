51ad83d5005f25bc40b5a21047c684ddf667107a
notThere <klappstuhl@schoschonen.at>
2020-05-09 14:00:18 +0200

    products per category added (tested)

--- WebProject/Category.cs
+++ WebProject/Category.cs
@@ -11,7 +11,8 @@ namespace WebProject
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Linq;
+
     public partial class Category
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,6 +21,7 @@ namespace WebProject
             this.Products = new HashSet<Product>();
         }
     
+        public int Count { get { return (from t in Products where t.Category.CategoryID == CategoryID select t).ToList().Count; } }
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
